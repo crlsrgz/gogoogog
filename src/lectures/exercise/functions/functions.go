cbd0f1d8cf6fcec15e6f854ad5b18fe5a11fb7b1
crlsrgz <course@carlosfx.com>
2024-02-02 09:35:04 +0100

    func

--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -17,6 +17,40 @@
 
 package main
 
+import "fmt"
+
+func personName(name string) string {
+	return name
+}
+
+func textMessage(textMessage string) string {
+	return textMessage
+}
+
+func addNumbers(a, b, c int) int {
+	return a + b + c
+}
+
+func returnNumber(n int) int {
+	return n
+}
+
+func returnTwoNumbers(a, b int) (int, int) {
+	return a, b
+}
+func returnOtherTwoNumbers() (int, int) {
+	return 2, 7
+}
+func addThreeNumbers(a, b, c int) int {
+	return a + b + c
+}
+
 func main() {
+	fmt.Println("personName: ", personName("Peter"))
+	fmt.Println("textMessage: ", textMessage("Good day"))
+	fmt.Println("addNumbers: ", addNumbers(1, 2, 4))
+	fmt.Println("returnNumber: ", returnNumber(7))
+	fmt.Println(returnTwoNumbers(5, 7))
+	fmt.Println("addThreeNumbers: ", addThreeNumbers(returnNumber(2), 3, 5))
 
 }
