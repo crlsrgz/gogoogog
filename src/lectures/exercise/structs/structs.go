b335d3834d20aebb9ddd882372234ff2c7a166a9
crlsrgz <course@carlosfx.com>
2024-02-06 16:47:30 +0100

    struct rectangle

--- src/lectures/exercise/structs/structs.go
+++ src/lectures/exercise/structs/structs.go
@@ -17,8 +17,56 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+func input(valueName string) int {
+	var input string
+	var number int
+
+	for {
+		fmt.Print("Whole number for ", valueName, ": ")
+		_, err := fmt.Scan(&input)
+		if err != nil {
+			fmt.Println("Invalid input, Whole number for ", valueName, ": ")
+			continue
+		}
+
+		number, err = strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Not a Whole number, Whole number for ", valueName, ": ")
+			continue
+		}
+
+		// Successful conversion, exit the loop
+		break
+	}
+	return number
+	// fmt.Printf(" Integer = %d\n", number)
+}
+
+type Rectangle struct {
+	Width     int
+	Length    int
+	Perimeter int
+	Area      int
+}
 
 func main() {
+	widthValue := input("width")
+	lengthValue := input("length")
+	//fmt.Println(widthValue, lengthValue)
+
+	rectangleOne := Rectangle{}
+	rectangleOne.Width = widthValue
+	rectangleOne.Length = lengthValue
+
+	rectangleArea := rectangleOne.Width * rectangleOne.Length
+	rectanglePerimeter := (rectangleOne.Width * 2) + (rectangleOne.Length * 2)
+	rectangleOne.Area = rectangleArea
+	rectangleOne.Perimeter = rectanglePerimeter
+	fmt.Println(rectangleOne, " Perimeter: ", rectanglePerimeter, "; ", "Area: ", rectangleArea)
 
 }
