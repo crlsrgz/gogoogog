f621a83d95d2d807501a60e9bc0134a10e6d7c61
crlsrgz <course@carlosfx.com>
2024-02-14 12:45:26 +0100

    iota

--- src/lectures/demo/iota/iota.go
+++ src/lectures/demo/iota/iota.go
@@ -11,21 +11,24 @@ const (
 	West
 )
 
+// func (d Direction) String() string {
+// 	switch d {
+// 	case North:
+// 		return fmt.Sprintf("Going North")
+// 	case South:
+// 		return fmt.Sprintf("Going South")
+// 	case East:
+// 		return fmt.Sprintf("Going East")
+// 	case West:
+// 		return fmt.Sprintf("Going West")
+// 	default:
+// 		return "Other direction"
+// 	}
+// }
+/* 💡 Folowing pattern muss have the same order as the const list */
 func (d Direction) String() string {
-	switch d {
-	case North:
-		return fmt.Sprintf("Going North")
-	case South:
-		return fmt.Sprintf("Going South")
-	case East:
-		return fmt.Sprintf("Going East")
-	case West:
-		return fmt.Sprintf("Going West")
-	default:
-		return "Other direction"
-	}
+	return []string{"North", "East", "South", "West"}
 }
-
 func main() {
 	north := North
 	fmt.Println(north)
