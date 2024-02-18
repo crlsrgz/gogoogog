4a998bfcba0583ee7720de10763febacc80a23da
crlsrgz <crls@fake>
2024-02-18 20:03:58 +0100

    interfaces, demo

--- src/lectures/demo/interfaces/interfaces.go
+++ src/lectures/demo/interfaces/interfaces.go
@@ -2,6 +2,58 @@ package main
 
 import "fmt"
 
-func main() {
+// explanation
+type Resetter interface {
+	Reset()
+}
+type Coordinate struct {
+	x int
+	y int
+}
+type Player struct {
+	health   int
+	position Coordinate
+}
 
+func (p *Player) Reset() {
+	p.health = 100
+	p.position = Coordinate{0, 0}
+}
+
+func Reset(r Resetter) {
+	r.Reset()
+}
+
+// demo
+
+type Preparer interface {
+	PrepareDish()
+}
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("cook chicken")
+}
+func (s Salad) PrepareDish() {
+	fmt.Println("chop salad")
+	fmt.Println("add dressing")
+}
+func prepareDishes(dishes []Preparer) {
+	fmt.Println("Prepare dishes:")
+	for i := 0; i < len(dishes); i++ {
+		dish := dishes[i]
+		fmt.Printf("==Dish: %v==\n", dish)
+		dish.PrepareDish()
+	}
+	fmt.Println()
+}
+func main() {
+	player := Player{50, Coordinate{10, 10}}
+	fmt.Println(player)
+	Reset(&player)
+	fmt.Println(player)
+
+	dishes := []Preparer{Chicken("Chicken Wings"), Salad("Ceasar")}
+	prepareDishes(dishes)
 }
--- src/lectures/exercise/interfaces/interfaces.go
+++ src/lectures/exercise/interfaces/interfaces.go
@@ -23,4 +23,5 @@ import "fmt"
 
 func main() {
 
+
 }
