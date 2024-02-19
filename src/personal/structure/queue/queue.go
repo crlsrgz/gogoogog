c7f923d30d9d8504a3f0407bb9e88880f4f02f4c
crlsrgz <course@carlosfx.com>
2024-02-19 14:23:13 +0100

    per: queue tests

--- src/personal/structure/queue/queue.go
+++ src/personal/structure/queue/queue.go
@@ -1,27 +1,30 @@
 package main
 
-import "fmt"
-type QNodeValuer interface {}
+import (
+	"fmt"
+)
+
+type QNodeValuer interface{}
 
 type QNode struct {
 	value QNodeValuer
-	next *QNode
+	next  *QNode
 }
 
-type Queue struct{
-	head *QNode
-	tail *QNode
+type Queue struct {
+	head   *QNode
+	tail   *QNode
 	length int
 }
+
 func (q *Queue) enqueue(value QNodeValuer) {
 	node := QNode{}
 	node.value = value
 	q.length++
-	
 
 	if q.tail == nil {
-		q.head =  &node
-		q.tail = q.head 
+		q.head = &node
+		q.tail = q.head
 		return
 	}
 
@@ -30,7 +33,7 @@ func (q *Queue) enqueue(value QNodeValuer) {
 
 }
 
-func (q *Queue) dequeue() QNodeValuer{
+func (q *Queue) dequeue() QNodeValuer {
 	if q.head == nil {
 		fmt.Println("error: Empty Queue")
 		return -1
@@ -38,45 +41,65 @@ func (q *Queue) dequeue() QNodeValuer{
 	q.length--
 	head := q.head
 	q.head = q.head.next
-	
+
 	return head.value
 
 }
+func (q *Queue) peek() {
+	if q.head != nil {
 
-
+		fmt.Printf("Head value is: %v\n", q.head.value)
+	}
+	fmt.Printf("Empty 'Queue")
+}
+func (q *Queue) dumpQueue() {
+	tempNode := q.head
+	// fmt.Println(tempNode.next)
+	for {
+		fmt.Printf("-> %v ", tempNode.value)
+		tempNode = tempNode.next
+		if tempNode == nil {
+			fmt.Println()
+			return
+		}
+	}
+}
 
 func main() {
 	fmt.Println("Init")
-	
+
 	intNode := QNode{10, nil}
 	stringNode := QNode{"ten", nil}
-	
+
 	fmt.Println("testing nodes", intNode, stringNode)
 
-	fmt.Println("////");
+	fmt.Println("////")
 	myQueue := Queue{}
 	fmt.Println(myQueue)
 	fmt.Println("Queue length: ", myQueue.length)
-	fmt.Println("////-1");
+	fmt.Println("////-1")
 	myQueue.dequeue()
-	fmt.Println("////");
+	fmt.Println("////")
 	myQueue.enqueue(4)
 	fmt.Println("Queue length: ", myQueue.length)
 	fmt.Println(myQueue.tail)
-	fmt.Println("////+1");
+	fmt.Println("////+1")
 	myQueue.enqueue("four")
 	fmt.Println("Queue length: ", myQueue.length)
 	fmt.Println(myQueue.tail)
-	fmt.Println("////+2");
+	fmt.Println("////+2")
 	myQueue.enqueue(5)
 	myQueue.enqueue("five")
 	fmt.Println("Queue length: ", myQueue.length)
 	fmt.Println(myQueue.tail)
-	fmt.Println("////deq*2");
-	fmt.Println(myQueue.dequeue())
-	fmt.Println(myQueue.dequeue())
+	fmt.Println("////deq*2")
+	// fmt.Println(myQueue.dequeue())
+	// fmt.Println(myQueue.dequeue())
+	myQueue.peek()
 	fmt.Println("Queue length: ", myQueue.length)
 	fmt.Println(myQueue.head, myQueue.tail)
-	
+
+	fmt.Println("////dump")
+	myQueue.dumpQueue()
 
 }
--- src/personal/structure/queue/queue_test.go
+++ src/personal/structure/queue/queue_test.go
@@ -1,26 +1,49 @@
-package main 
+package main
 
-import "testing"
-import "reflect"
+import (
+	"reflect"
+	"testing"
+)
 
-func qnodeInt(value int) QNode{
+func qnodeInt(value int) QNode {
 	return QNode{value, nil}
 
 }
-func qnodeString(value string) QNode{
+func qnodeString(value string) QNode {
 	return QNode{value, nil}
 
 }
+func createQueue() Queue {
 
-func TestQNodes(t *testing.T){
+	myQueue := Queue{}
+	myQueue.enqueue(4)
+	myQueue.enqueue("four")
+	myQueue.enqueue(5)
+	myQueue.enqueue("five")
+
+	return myQueue
+}
+func TestQueue(t *testing.T) {
+	myQueue := createQueue()
+	if myQueue.head.value != 3 {
+		t.Logf("Queue head is %v, want 4 (int)", myQueue.head.value)
+	}
+
+	if myQueue.tail.value != 4 {
+		t.Fatalf("Queue head is %v, want five(string))", myQueue.tail.value)
+	}
+	myQueue.dumpQueue()
+}
+
+func TestQNodes(t *testing.T) {
 	var intVar int
-	node := qnodeInt(10) 
-	if reflect.TypeOf(node.value) != reflect.TypeOf(intVar){
+	node := qnodeInt(10)
+	if reflect.TypeOf(node.value) != reflect.TypeOf(intVar) {
 
 		t.Logf("Node value is not an int, value: %v, learn about types in go", node.value)
 	}
 	if node.value != 10 {
 		t.Fatalf("Node value is %v, want 10 type int", node.value)
 	}
-	
+
 }
