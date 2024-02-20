7ee1d5c45f3e10375b9391b134f0d6da8c4db47a
crlsrgz <course@carlosfx.com>
2024-02-20 22:00:03 +0100

    les: error in win

--- src/lectures/demo/errors/errors.go
+++ src/lectures/demo/errors/errors.go
@@ -4,23 +4,25 @@ import (
 	"errors"
 	"fmt"
 )
+
 type Stuff struct {
 	values []int
 }
-func (stuff *Stuff) Get(index int) (int, error){
-	if index > len(stuff.values){
+
+func (s *Stuff) Get(index int) (int, error) {
+	if index > len(s.values) {
 		return 0, errors.New(fmt.Sprintf("no element at index %v", index))
-	}else {
-		return stuff.values[index], nil
+	} else {
+		return s.values[index], nil
 	}
 }
+
 func main() {
-	fmt.Println("//////")
 	stuff := Stuff{}
 	value, err := stuff.Get(1)
 	if err != nil {
 		fmt.Println(err)
-	}else {
-		fmt.Println("value=", value)
+	} else {
+		fmt.Println("value is", value)
 	}
 }
