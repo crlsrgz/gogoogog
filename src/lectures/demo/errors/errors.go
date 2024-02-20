332a7da961af0bb296520986730a3f085cdd3ff3
crlsrgz <course@carlosfx.com>
2024-02-20 13:35:17 +0100

    les: error

--- src/lectures/demo/errors/errors.go
+++ src/lectures/demo/errors/errors.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
+func divide(lhs, rhs int) (int, error) {
+	if rhs == 0 {
+		return 0, errors.New("cannot divide by zero")
+	} else {
+		return rhs / lhs, nil
+	}
+}
+
 func main() {
+	fmt.Println("//////")
+	fmt.Println(divide(10, 200))
 
 }
