db9f3fbb55c157b110e7260fd6d8157e3e7c0c25
crlsrgz <course@carlosfx.com>
2024-02-22 21:34:39 +0100

    les: Generics

--- src/lectures/demo/generics/generics.go
+++ src/lectures/demo/generics/generics.go
@@ -3,10 +3,48 @@ package main
 import "fmt"
 
 const (
-	Low    = iota
+	Low = iota
 	Medium
 	High
 )
 
+/* 💡 The convention is to use T for generics, this case uses P */
+type PriorityQueue[P comparable, V any] struct {
+	items      map[P][]V
+	priorities []P
+}
+
+func NewPriorityQueue[P comparable, V any](priorities []P) PriorityQueue[P, V] {
+	return PriorityQueue[P, V]{items: make(map[P][]V), priorities: priorities}
+}
+
+func (pq *PriorityQueue[P, V]) Add(priority P, value V) {
+	pq.items[priority] = append(pq.items[priority], value)
+}
+
+func (pq *PriorityQueue[P, V]) Next() (V, bool) {
+	for i := 0; i < len(pq.priorities); i++ {
+		priority := pq.priorities[i]
+		items := pq.items[priority]
+		if len(items) > 0 {
+
+			next := items[0]
+			pq.items[priority] = items[i:]
+			return next, true
+		}
+	}
+	var empty V
+	return empty, false
+}
+
 func main() {
+	queue := NewPriorityQueue[int, string]([]int{High, Medium, Low})
+	queue.Add(Low, "L-1")
+	queue.Add(High, "H-1")
+	fmt.Println(queue.Next())
+	queue.Add(Medium, "M-1")
+	queue.Add(High, "H-2")
+	fmt.Println(queue.Next())
+	fmt.Println(queue.Next())
+	fmt.Println(queue.Next())
 }
--- src/lectures/exercise/embedding/embedding.go
+++ src/lectures/exercise/embedding/embedding.go
@@ -31,8 +31,49 @@ type MemoryUsage struct {
 	amount []Bytes
 }
 
+func (b *BandwidthUsage) AverageBandW() int {
+	sum := 0
+	for i := 0; i < len(b.amount); i++ {
+		sum += int(b.amount[i])
+	}
+	return sum / len(b.amount)
+}
+
+func (c *CpuTemp) AverageCPUTemp() int {
+	sum := 0
+	for i := 0; i < len(c.temp); i++ {
+		sum += int(c.temp[i])
+	}
+
+	return sum / len(c.temp)
+}
+
+func (m *MemoryUsage) AverageMemoryUsage() int {
+	sum := 0
+	for i := 0; i < len(m.amount); i++ {
+		sum += int(m.amount[i])
+	}
+
+	return sum / len(m.amount)
+}
+
+type Dashboard struct {
+	BandwidthUsage
+	CpuTemp
+	MemoryUsage
+}
+
 func main() {
 	bandwidth := BandwidthUsage{[]Bytes{50000, 100000, 130000, 80000, 90000}}
 	temp := CpuTemp{[]Celcius{50, 51, 53, 51, 52}}
 	memory := MemoryUsage{[]Bytes{800000, 800000, 810000, 820000, 800000}}
+
+	dashboard := Dashboard{
+		BandwidthUsage: bandwidth,
+		CpuTemp:        temp,
+		MemoryUsage:    memory,
+	}
+	fmt.Printf("Avg. bandwidth is %v", dashboard.AverageBandW())
+	fmt.Printf("Avg. temp is %v", dashboard.AverageCPUTemp())
+	fmt.Printf("Avg. memory usage is %v", dashboard.AverageMemoryUsage())
 }
