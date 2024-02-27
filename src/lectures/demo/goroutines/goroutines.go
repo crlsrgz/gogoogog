1bab7088f28d329ea00752d298e1c0f9ae8ae252
crlsrgz <course@carlosfx.com>
2024-02-27 13:28:54 +0100

    les: goroutines

--- src/lectures/demo/goroutines/goroutines.go
+++ src/lectures/demo/goroutines/goroutines.go
@@ -1,9 +1,42 @@
 package main
 
-import "fmt"
-import "time"
-import "unicode"
+import (
+	"fmt"
+	"time"
+	"unicode"
+)
 
 func main() {
+
 	data := []rune{'a', 'b', 'c', 'd'}
+	var capitalized []rune
+
+	capitalizeIt := func(r rune) {
+		capitalized = append(capitalized, unicode.ToUpper(r))
+		fmt.Printf("%c done!\n", r)
+
+	}
+	for i := 0; i < len(data); i++ {
+		go capitalizeIt(data[i])
+	}
+	time.Sleep(20 * time.Millisecond)
+	fmt.Printf("Capitalized: %c \n", capitalized)
+	/* ═══  ═══ */
+	fmt.Printf("\n\n/////////\n")
+	var counter int = 0
+	wait := func(ms time.Duration) {
+		time.Sleep(ms * time.Millisecond)
+		counter += 1
+	}
+	fmt.Println("Launch goroutines")
+	fmt.Println(100 * time.Millisecond)
+
+	go wait(100)
+	go wait(900)
+	go wait(1000)
+
+	fmt.Println("launched counter=", counter)
+	time.Sleep(1100 * time.Millisecond)
+	fmt.Println("waited 1100ms. counter=", counter)
+
 }
