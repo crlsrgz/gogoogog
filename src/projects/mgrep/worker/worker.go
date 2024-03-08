d29d35a30ff81500dacac59d9eced4ea7044a360
crlsrgz <course@carlosfx.com>
2024-03-08 21:56:59 +0100

    les: mgrep final

--- src/projects/mgrep/worker/worker.go
+++ src/projects/mgrep/worker/worker.go
@@ -21,7 +21,7 @@ func NewResult(line string, lineNum int, path string) Result {
 	return Result{line, lineNum, path}
 }
 
-func FindInFile(path string, find string) *Result {
+func FindInFile(path string, find string) *Results {
 	file, err := os.Open(path)
 	if err != nil {
 		fmt.Println("Errot", err)
